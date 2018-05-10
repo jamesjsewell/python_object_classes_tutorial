182e41114870246b331f125d9300049a486b0cb9
james sewell <jamesjosephsewell@gmail.com>
2018-05-10 16:08:29 -0500

    added a house builder feature that lets the user specify the features of the house

--- house_program_classes.py
+++ house_program_classes.py
@@ -1,4 +1,3 @@
-
 # room class takes in user defined room data and stores it on itself
 class Room:
 
@@ -18,27 +17,20 @@ class Room:
 
 # house class takes room data and creates those rooms using the room class
 # then stores the houses's room objects on itself in an array. Also takes in other house data
-class House:
+class House():
 
-    def __init__(self, house_data):
+    def __init__(self):
 
         #assigns the values to self from the house_data dictionary passed in for the particular house 
-        self.address = house_data['address']
-        self.price = house_data['price']
+        self.address = None
+        self.price = None
         self.rooms = []
+        self.color = None
+        self.roof_material = None
+        self.has_garage = None
         self.square_feet = None
-        self.color = house_data['color']
-        self.roof_material = house_data['roof_material']
-        self.has_garage = house_data['has_garage']
-        self.rooms_data = house_data['rooms_data']
-        self.Room_class = Room
-
-        #spawns rooms and stores them on self
-        self.create_rooms()
-
-        # loops through self.rooms and totals up each rooms square feet
-        self.calculate_house_size()
-
+        self.rooms_data = None
+      
     # this class iterates through the room data supplied to the house class
     # and spawns instances of rooms using the room class
     # then stores the rooms on the house class via self
@@ -82,6 +74,31 @@ class House:
 
         return (f"\n address: {self.address} \n price: {self.price} \n square feet: {self.square_feet} \n color: {self.color} \n roof: {self.roof_material} \n has garage? {self.has_garage} \n rooms: {self.rooms} \n")
 
+class House_Builder():
+
+    def __init__(self):
+
+        self.house = House()
+        self.creat_house()      
+        
+    def creat_house(self):
+
+        house_attributes = ['address',
+        'price',
+        'rooms',
+        'color',
+        'roof_material',
+        'has_garage']
+
+        for house_attribute in house_attributes:
+
+            user_input = input(f"enter {house_attribute}: ")
+            setattr(self.house, house_attribute, user_input)
+
+
+        print(self.house)
+
+
 # this class serves as the main logic container of the app
 class Neighboorhood_Program:
 
@@ -89,18 +106,19 @@ class Neighboorhood_Program:
     # is spawned. It takes in the house data in the addresses argument
     def __init__(self, addresses):
         
-        # applies the addresses ( house_data ) to the instance of the class
-        self.addresses = addresses
+        # # applies the addresses ( house_data ) to the instance of the class
+        # self.addresses = addresses
 
-        # loops through each address or house in the self.addresses dictionary
-        for address in self.addresses:
+        # # loops through each address or house in the self.addresses dictionary
+        # for address in self.addresses:
 
-            # this variable becomes the house data value for each particular house key in the addresses dictionary
-            # so house_data is its own dictionary that contains all of the data for a house
-            house_data = self.addresses[address]
+        #     # this variable becomes the house data value for each particular house key in the addresses dictionary
+        #     # so house_data is its own dictionary that contains all of the data for a house
+        #     house_data = self.addresses[address]
 
-            # runs the add_a_new_house method and passes in the house data dictionary
-            self.add_a_new_house(house_data)
+        #     # runs the add_a_new_house method and passes in the house data dictionary
+        #     self.add_a_new_house(house_data)
+        house_builder = House_Builder()
 
     # creats a house
     def add_a_new_house(self, house_data):
